bef2211c70c6adefaddb87e16e93103b8bda3efa
krito <kritomania1991@gmail.com>
2024-11-07 20:36:09 +0300

    hw_10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lasName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        //Задача 2
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        //Задача 3
+        fullName = "Иванов Семён Семёнович";
+        String fullNameCorrected = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullNameCorrected);
     }
 }
